6199274ab4b481ee57e3f2a91935dc2bb76d004a
Akash kumar <94979388+iakash22@users.noreply.github.com>
2024-09-12 02:52:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './App.css';
+import { LuLoader } from 'react-icons/lu';
+const App = () => {
+  return (
+    <div style={{
+      width : "100vw",
+      height : "100vh",
+      display : "flex",
+      flexDirection : "column",
+      alignItems : "center",
+      justifyContent: "center",
+      gap: "2rem",
+      backgroundColor: "black",
+      color : "white"
+    }}>
+      <img
+        src="./logo.jpeg"
+        alt="logo"
+        width={"200px"}
+        height={"150px"}
+        style={{
+          maxWidth: "250px",
+          maxHeight : "200px"
+        }}
+      />
+      <p style={{fontSize : "2.5rem", fontWeight : 600, textAlign : "center"}}>Website is Under maintenance.</p>
+      <LuLoader
+        className='animte-spin'
+        style={{
+          fontSize: "5rem",
+          color : "#478CCF"
+        }}
+      />
+    </div>
+  )
+}
+
+export default App
